dd546036e921e20f36c1f2d0d777fc8e120d540e
CodewithNiki <101751820+CodewithNiki@users.noreply.github.com>
2023-11-01 21:25:11 +0100

    update

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
