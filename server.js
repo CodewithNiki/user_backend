2e09c9ef0a40a39808cc0c5f2d37f83c5a338772
CodewithNiki <101751820+CodewithNiki@users.noreply.github.com>
2023-11-02 11:14:03 +0100

    update

--- server.js
+++ server.js
@@ -11,19 +11,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Define the origins that are allowed to access your server
-const allowedOrigins = ['http://localhost:5173', 'https://your-frontend-domain.com'];
 
 // CORS middleware configuration
 app.use(cors({
-  origin: function (origin, callback) {
-    // Allow requests with no origin (like mobile apps or curl requests)
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
-}));
+    origin: 'http://localhost:5173', // Update this to match your frontend URL
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true, // Include credentials, if needed (e.g., cookies)
+    optionsSuccessStatus: 204, // An HTTP status to respond with for preflight requests
+  }));
+  
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoutes"));
 app.use("/api/users", require("./routes/userRoutes"));
