75b2a584bd07b9ff8d3eb3cb0d453a2222b0479b
CodewithNiki <101751820+CodewithNiki@users.noreply.github.com>
2023-11-21 19:53:18 +0100

    update

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 5000;
 // CORS middleware configuration
 app.use(cors());
 app.use((req, res, next)=>{
-  res.header("Access-Control-Allow-Origin", ["*", "http//localhost:5173/contacts/contactForm", "https://users-crudapp.onrender.com/", "https://users-crudapp.onrender.com/contacts", "https://users-crudapp.onrender.com/register", "https://users-crudapp.onrender.com/login"]);
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, HEAD, OPTIONS");
   res.header("Access-Control-Allow-Headers", "Origins, X-Requested-With, Content-Type, Accept, Authorization");
   next();
